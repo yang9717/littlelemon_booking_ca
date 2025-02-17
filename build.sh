b535acf7f2abfe63b9bcb10a841a8729bcab6a7e
xyan0152 <xyan0152@student.monash.edu>
2025-02-17 15:53:29 +0800

    for render deployment

--- build.sh
+++ build.sh
@@ -4,5 +4,12 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# To create Django admin account
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
+
 python manage.py collectstatic --no-input
 python manage.py migrate
