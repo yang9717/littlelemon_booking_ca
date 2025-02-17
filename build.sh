cf316efa38b826811fa85442be1501ebaf72b424
xyan0152 <xyan0152@student.monash.edu>
2025-02-17 16:15:14 +0800

    modified CREATE_SUPERUSER

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ pip install -r requirements.txt
 # To create Django admin account
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input
+  python manage.py createsuperuser \
+    --noinput \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
 fi
 
 python manage.py collectstatic --no-input
